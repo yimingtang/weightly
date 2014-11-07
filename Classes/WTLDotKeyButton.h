3a239230aacd289f5be727c8b85752256ed2cd40
Yiming Tang <yimingnju@gmail.com>
2014-11-07 21:25:01 +0800

    Hack the number pad

---
+++ Classes/WTLDotKeyButton.h
@@ -0,0 +1,13 @@
+//
+//  WTLDotKeyButton.h
+//  Weightly
+//
+//  Created by Yiming Tang on 11/7/14.
+//  Copyright (c) 2014 Yiming Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTLDotKeyButton : UIButton
+
+@end
--- Classes/WTLTextField.m
+++ Classes/WTLTextField.m
@@ -7,6 +7,13 @@
 //
 
 #import "WTLTextField.h"
+#import "WTLDotKeyButton.h"
+
+@interface WTLTextField ()
+
+@property (nonatomic, strong) WTLDotKeyButton *dotKeyButton;
+
+@end
 
 @implementation WTLTextField
 
@@ -18,9 +25,33 @@
     if (!_unitLabel) {
         _unitLabel = [[UILabel alloc] initWithFrame:CGRectZero];
         _unitLabel.textColor = [UIColor colorWithWhite:1.0f alpha:0.5f];
-        _unitLabel.font = [UIFont fontWithName:@"AvenirNext-UltraLight" size:30.0f];
+        _unitLabel.font = [UIFont fontWithName:@"Avenir-Light" size:30.0f];
     }
     return _unitLabel;
+}
+
+
+- (WTLDotKeyButton *)dotKeyButton {
+    if (!_dotKeyButton) {
+        CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
+        if (UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation)) {
+            _dotKeyButton = [[WTLDotKeyButton alloc] initWithFrame:CGRectMake(0, 122.0f, roundf(screenWidth / 3) - 3.0f, 40.0f)];
+        } else {
+            _dotKeyButton = [[WTLDotKeyButton alloc] initWithFrame:CGRectMake(0, 163.0f, roundf(screenWidth / 3) - 3.0f, 53.0f)];
+        }
+        [_dotKeyButton addTarget:self action:@selector(dotKeyClicked:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _dotKeyButton;
+}
+
+
+#pragma mark - NSObject
+
+- (void)dealloc {
+    if (UI_USER_INTERFACE_IDIOM() != UIUserInterfaceIdiomPad) {
+        [[NSNotificationCenter defaultCenter] removeObserver:self];
+        [self.dotKeyButton removeFromSuperview];
+    }
 }
 
 
@@ -75,6 +106,63 @@
     self.rightViewMode = UITextFieldViewModeAlways;
     self.textEdgeInsets = UIEdgeInsetsMake(8.0f, 16.0f, 8.0f, 16.0f);
     self.unitLabelEdgeInsets = UIEdgeInsetsMake(8.0f, 0.0f, 8.0f, 16.0f);
+    
+    if (UI_USER_INTERFACE_IDIOM() != UIUserInterfaceIdiomPad) {
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardDidHide:) name:UIKeyboardDidHideNotification object:nil];
+    }
+}
+
+
+#pragma mark - Actions
+
+- (void)dotKeyClicked:(id)sender {
+    [self insertText:@"."];
+}
+
+
+#pragma mark - Handle Notifications
+
+- (void)keyboardWillShow:(NSNotification *)notification {
+    // Hack the system number pad
+    NSArray *windows = [[UIApplication sharedApplication] windows];
+    if (windows.count < 2) {
+        return;
+    }
+    
+    UIWindow *keyboardWindow = [windows objectAtIndex:1];
+    UIView *keyboardView = nil;
+    
+    // Find the keyboard view
+    for (UIView *view in keyboardWindow.subviews) {
+        if ([view.description hasPrefix:@"<UIInputSetContainerView"]) {
+            for (UIView *subView in view.subviews) {
+                if ([subView.description hasPrefix:@"<UIInputSetHostView"]) {
+                    keyboardView = subView;
+                }
+            }
+        }
+    }
+    
+    if (!keyboardView) {
+        return;
+    }
+    
+    NSDictionary *userInfo = notification.userInfo;
+    CGSize keyboardSize = [[userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    // Add dot key if necessary
+    if (!self.dotKeyButton.superview) {
+        [keyboardView addSubview:self.dotKeyButton];
+    }
+    CGFloat keyWidth = roundf(keyboardSize.width / 3) - 3.0f;
+    CGFloat keyHeight = roundf(keyboardSize.height / 4) - 1.0f;
+    self.dotKeyButton.frame = CGRectMake(0.0f, keyboardSize.height - keyHeight, keyWidth, keyHeight);
+}
+
+
+- (void)keyboardDidHide:(NSNotification *)notification {
+    [self.dotKeyButton removeFromSuperview];
 }
 
 @end
