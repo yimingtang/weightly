af2dddaf423b0caab68e07168927f55a269a4b33
Yiming Tang <yimingnju@gmail.com>
2014-11-12 18:22:05 +0800

    Start developing unit converter

---
+++ Classes/WTLUnitConverter.h
@@ -0,0 +1,13 @@
+//
+//  WTLUnitConverter.h
+//  Weightly
+//
+//  Created by Yiming Tang on 11/12/14.
+//  Copyright (c) 2014 Yiming Tang. All rights reserved.
+//
+
+@import Foundation;
+
+@interface WTLUnitConverter : NSObject
+
+@end
---
+++ Classes/WTLUnitConverter.m
@@ -0,0 +1,13 @@
+//
+//  WTLUnitConverter.m
+//  Weightly
+//
+//  Created by Yiming Tang on 11/12/14.
+//  Copyright (c) 2014 Yiming Tang. All rights reserved.
+//
+
+#import "WTLUnitConverter.h"
+
+@implementation WTLUnitConverter
+
+@end
