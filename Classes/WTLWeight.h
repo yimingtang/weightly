8174738c59f330b1dc17e63fb074e1b9870f1a52
Yiming Tang <yimingnju@gmail.com>
2014-11-13 14:22:32 +0800

    Add models

---
+++ Classes/WTLWeight.h
@@ -0,0 +1,13 @@
+//
+//  WTLWeight.h
+//  Weightly
+//
+//  Created by Yiming Tang on 11/13/14.
+//  Copyright (c) 2014 Yiming Tang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WTLWeight : NSObject
+
+@end
---
+++ Classes/WTLWeight.m
@@ -0,0 +1,13 @@
+//
+//  WTLWeight.m
+//  Weightly
+//
+//  Created by Yiming Tang on 11/13/14.
+//  Copyright (c) 2014 Yiming Tang. All rights reserved.
+//
+
+#import "WTLWeight.h"
+
+@implementation WTLWeight
+
+@end
